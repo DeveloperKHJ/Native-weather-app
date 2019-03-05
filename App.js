2a63ebeb78427213b2b48e0a68d1355dde082f54
DeveloperKHJ <dbtmd1007gh1@gmail.com>
2019-03-05 10:03:35 +0900

    -

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View, ActivityIndicator} from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Awesome! jerk off love it</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
